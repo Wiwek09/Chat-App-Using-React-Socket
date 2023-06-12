a7efc4c257f85d6957f5aec482c083997c756819
Wiwek09 <bibekwiwek54@gmail.com>
2023-06-12 17:39:53 +0545

    New

---
+++ client/build/precache-manifest.5a106f1466bb6c6281d3e546fb86b8d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a78c28763fb8a0e883e5",
+    "url": "/static/css/main.cbbb2368.chunk.css"
+  },
+  {
+    "revision": "a78c28763fb8a0e883e5",
+    "url": "/static/js/main.a78c2876.chunk.js"
+  },
+  {
+    "revision": "9bcdb338c956eb72db25",
+    "url": "/static/js/1.9bcdb338.chunk.js"
+  },
+  {
+    "revision": "ddfa2c5329c881a7ef67",
+    "url": "/static/js/2.ddfa2c53.chunk.js"
+  },
+  {
+    "revision": "d9b1b9ef5c7fba5e2668",
+    "url": "/static/js/runtime~main.d9b1b9ef.js"
+  },
+  {
+    "revision": "46ae6153d89c20a24c8fe200aba3e299",
+    "url": "/index.html"
+  }
+];
